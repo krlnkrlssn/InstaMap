1d508f4b02855ef8b6e2ed6ac096a85affb59d1f
krlnkrlssn <krlnkrlssn@gmail.com>
2017-05-04 12:08:31 +0200

    Style och fix

--- www/scripts/scripts.js
+++ www/scripts/scripts.js
@@ -1,15 +1,11 @@
 
+   var showDialog = function(id) {
+      document.getElementById(id).show();
+      }
 
-    function openDialog(id) {
-      document.getElementById(id);
-        //.show();
-
-    };
-
-    function hideDialog(id) {
-      document.getElementById(id);
-      //.hide();
-    };
+    var hideDialog = function(id) {
+      document.getElementById(id).hide();
+    }
 
     function showPage(page) {
       console.log("showpage " + page)
@@ -21,13 +17,13 @@
     function openMenu() {
       var menu = document.getElementById('menu');
       menu.open();
-    };
+    }
 
     function loadPage(page) {
       var content = document.getElementById('content');
       var menu = document.getElementById('menu');
       content.load(page).then(menu.close.bind(menu));
-    };
+    }
 
 
 		function login(username,password) {
